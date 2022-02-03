31d4d97d170805e3888c71a95efe84edfd038465
thebear617 <98879632+thebear617@users.noreply.github.com>
2022-02-03 17:48:20 +0800

    Update 2.3上课.cpp

--- 2.3上课.cpp
+++ 2.3上课.cpp
@@ -16,6 +16,7 @@ int main()
 	char password[20]={0};
 	printf("请输入密码：");
 	scanf("%s",password);
+	getchar();
 	printf("请确认（Y/N）");
 	int ret;
 	ret=getchar();
@@ -28,6 +29,12 @@ int main()
 		printf("放弃确认\n");
 	}
 	//按照上述代码就会有直接出放弃确认
+	
+	//‘斜杠n'对应asc码的10
+	//引入缓冲区的讲解（图1）
+	//如何让缓冲区清0？用一个getchar让缓冲区的‘\n'读走，即清空缓冲区
+	
+	
 	
 	
 
