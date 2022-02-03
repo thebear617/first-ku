97be9698e3355d79658ea1cf7db6c97f4ba22cd1
thebear617 <98879632+thebear617@users.noreply.github.com>
2022-02-03 16:25:42 +0800

    Update 2.3上课.cpp

--- 2.3上课.cpp
+++ 2.3上课.cpp
@@ -6,12 +6,28 @@ int main()
 	printf("%c\n",ch);//初步理解getchar putchar
 	return 0;
 	int ch=0;*/
-	//代码2：eof理解
+	/*代码2：eof理解
 	int ch=0;
 	while((ch=getchar())!=EOF)
 {
 	putchar(ch);
-}
+}*/
+	/*代码3:*/
+	char password[20]={0};
+	printf("请输入密码：");
+	scanf("%s",password);
+	printf("请确认（Y/N）");
+	int ret;
+	ret=getchar();
+	if(ret=='Y')
+	{
+		printf("确认成功\n");
+	}
+	else
+	{
+		printf("放弃确认\n");
+	}
+	
 
 }
 
