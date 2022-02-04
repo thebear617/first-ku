0fafc41eb48e89f90cf7d306ff3dfd4785a09ea9
thebear617 <98879632+thebear617@users.noreply.github.com>
2022-02-04 13:43:03 +0800

    Update 上机练习1.cpp
    3个数和

--- 上机练习1.cpp
+++ 上机练习1.cpp
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,b;
+	/*int a,b;
 	scanf("%d %d",&a,&b);
 	printf("%d\n",a+b);
 		printf("%d\n",a-b);
 	printf("%d\n",a*b);
-	printf("%d\n",a/b);
+	printf("%d\n",a/b);*/
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	printf("%d\n",a+b+c);
 
 }
