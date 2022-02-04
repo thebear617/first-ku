83318991ea351e23096e5c8c450ddfa76630b455
thebear617 <98879632+thebear617@users.noreply.github.com>
2022-02-04 13:46:43 +0800

    Update 上机练习1.cpp
    长方体体积

--- 上机练习1.cpp
+++ 上机练习1.cpp
@@ -7,8 +7,12 @@ int main()
 		printf("%d\n",a-b);
 	printf("%d\n",a*b);
 	printf("%d\n",a/b);*/
-	int a,b,c;
+	/*int a,b,c;
 	scanf("%d %d %d",&a,&b,&c);
-	printf("%d\n",a+b+c);
+	printf("%d\n",a+b+c);*/
+	
+	/*float lenth,wide,high;
+	scanf("%f %f %f",&lenth,&wide,&high);
+		printf("%0.3f\n",lenth*wide*high);*/
 
 }
